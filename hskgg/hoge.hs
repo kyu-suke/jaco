4520f8850902e20c078d996130d871e4fcf316fa
kyu-suke <ykaneoka7@gmail.com>
2019-11-14 16:24:28 +0900

    🍥🍥🍥🍥🍥🍥🍥

--- hskgg/hoge.hs
+++ hskgg/hoge.hs
@@ -1,5 +1,4 @@
 
-
  
 -- データ型と値コンストラクタに同じ名前を使いました。
 -- 同じ名前にしておくことに当別な意味はないのですが、
@@ -77,3 +76,83 @@ infixr 5 ^++
 Empty ^++ ys = ys
 (x :-:xs) ^++ ys = x :-: (xs ^++ ys)
 
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a = Node a (treeInsert x left) right
+    | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a = treeElem x left
+    | x > a = treeElem x right
+
+nums = [8,2,4,9,1,3]
+sampleTree = foldr treeInsert EmptyTree nums
+
+-- 型クラスの定義
+class Eq2 a where
+  (^==) :: a -> a -> Bool
+  (^/=) :: a -> a -> Bool
+  x ^== y = not (x ^/= y)
+  x ^/= y = not (x ^== y)
+
+
+data TrafficLight = Red | Yellow | Green
+
+-- Eq型クラスのインスタンス
+instance Eq TrafficLight where
+    Red == Red = True
+    Yellow == Yellow = True
+    Green == Green = True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "this is Rded"
+    show Yellow = "this is Yellow"
+    show Green = "this is Green"
+
+-- 型クラスのサブクラス
+-- eg Num
+-- class (Eq a) => Num s where ...
+
+-- 多層型を型クラスのインスタンスにする
+-- eg. Maybe
+-- instance Eq (Maybe m) where ...
+-- mに対する等値比較も必要なのでmに対してクラス制約をつける
+-- instance (Eq m) => Eq (Maybe m) where ...
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno Nothing = False
+    yesno (Just _) = True
+
+instance YesNo (Tree a) where
+    yesno EmptyTree = False
+    yesno _ = True
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
